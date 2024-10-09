978c85fc5a7fd46480d1e8800dd95f6d3065bcb4
John <mwxfranco@gmail.com>
2024-10-09 13:02:04 +0100

    improved main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include <FloppyDiskController.h>
 
-#define DENSITY_PIN 14       // IDC 2  - OUTPUT
-#define INDEX_PIN 21         // IDC 8  - INPUT
-#define DRIVE_SELECT_PIN 32  // IDC 12 - OUTPUT
-#define MOTOR_ENABLE_PIN 15  // IDC 16 - OUTPUT
-#define DIR_PIN 33           // IDC 18 - OUTPUT
-#define STEP_PIN 27          // IDC 20 - OUTPUT
-#define WRDATA_PIN 12        // IDC 22 - OUTPUT
-#define WRGATE_PIN 13        // IDC 24 - OUTPUT
-#define TRK0_PIN A0          // IDC 26 - INPUT
-#define PROT_PIN A3          // IDC 28 - INPUT
-#define READ_PIN A1          // IDC 30 - INPUT
-#define SIDE_PIN A5          // IDC 32 - OUTPUT
-#define READY_PIN A2         // IDC 34 - INPUT
+#define DENSITY_PIN 14       // IDC 2  - ESP32 OUTPUT, IDC INPUT
+#define INDEX_PIN 21         // IDC 8  - ESP32 INPUT, IDC OUTPUT
+#define DRIVE_SELECT_PIN 32  // IDC 12 - ESP32 OUTPUT, IDC INPUT
+#define MOTOR_ENABLE_PIN 15  // IDC 16 - ESP32 OUTPUT, IDC INPUT
+#define DIR_PIN 33           // IDC 18 - ESP32 OUTPUT, IDC INPUT
+#define STEP_PIN 27          // IDC 20 - ESP32 OUTPUT, IDC INPUT
+#define WRDATA_PIN 12        // IDC 22 - ESP32 OUTPUT, IDC INPUT
+#define WRGATE_PIN 13        // IDC 24 - ESP32 OUTPUT, IDC INPUT
+#define TRK0_PIN A0          // IDC 26 - ESP32 INPUT, IDC OUTPUT
+#define PROT_PIN A3          // IDC 28 - ESP32 INPUT, IDC OUTPUT
+#define READ_PIN A1          // IDC 30 - ESP32 INPUT, IDC OUTPUT
+#define SIDE_PIN A5          // IDC 32 - ESP32 OUTPUT, IDC INPUT
+#define READY_PIN A2         // IDC 34 - ESP32 INPUT, IDC OUTPUT
 
 FloppyDrive floppy(DENSITY_PIN, INDEX_PIN, DRIVE_SELECT_PIN, MOTOR_ENABLE_PIN, DIR_PIN,
   STEP_PIN, WRDATA_PIN, WRGATE_PIN, TRK0_PIN, PROT_PIN, READ_PIN, SIDE_PIN, READY_PIN);
